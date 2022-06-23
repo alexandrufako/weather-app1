90c51ea011c8ffd3356461b381379e697ab4ba3c
alexandrufako <alexandru.fako@gmail.com>
2022-06-23 11:42:41 +0300

    added some components

---
+++ src/components/locationNav/locationNav.jsx
@@ -0,0 +1,11 @@
+function LocationNav = {
+    return(
+        <div className="nav-container">
+            <button className="location">ICON</button>
+            <p className="location-name">Bucharest</p>
+            <button className="menu-btn">MENU</button>
+        </div>
+    )
+}
+
+export default LocationNav;
