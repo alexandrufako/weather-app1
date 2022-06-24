a0cb1db19ae95a2b343b6bec246f9f9aa79ad2d7
alexandrufako <alexandru.fako@gmail.com>
2022-06-24 12:42:16 +0300

    footer menu switch done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,20 +1,105 @@
-import { Link } from 'react-router-dom'
-import { FiHome } from 'react-icons/fi' 
-import { FiSearch } from 'react-icons/fi' 
-import { MdAutoGraph } from 'react-icons/md' 
-import { FiSettings } from 'react-icons/fi' 
-import './footer.css'
+import React, { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
+import { FiHome } from "react-icons/fi";
+import { FiSearch } from "react-icons/fi";
+import { MdAutoGraph } from "react-icons/md";
+import { FiSettings } from "react-icons/fi";
+import "./footer.css";
 
 const Footer = () => {
+    const [select, setSelect] = useState({
+        home: "",
+        search: "",
+        forecast: "",
+        settings: "",
+    });
+
+    const selected = (param) => {
+        const tempObj = { ...select };
+
+        switch (param) {
+            case "home":
+                return (
+                    tempObj.home= "selected",
+                    tempObj.search= "",
+                    tempObj.forecast= "",
+                    tempObj.settings= "",
+                    setSelect(tempObj)
+                )
+        
+
+            case "search":
+                return (
+                    tempObj.home= "",
+                    tempObj.search= "search",
+                    tempObj.forecast= "",
+                    tempObj.settings= "",
+                    setSelect(tempObj)
+                )
+
+            case "forecast":
+                return (
+                    tempObj.home= "",
+                    tempObj.search= "",
+                    tempObj.forecast= "selected",
+                    tempObj.settings= "",
+                    setSelect(tempObj)
+                )
+
+            case "settings":
+                return (
+                    tempObj.home= "",
+                    tempObj.search= "",
+                    tempObj.forecast= "",
+                    tempObj.settings= "selected",
+                    setSelect(tempObj)
+                )
+
+            default:
+                return (
+                    tempObj.home= "",
+                    tempObj.search= "",
+                    tempObj.forecast= "",
+                    tempObj.settings= "",
+                    setSelect(tempObj)
+                );
+        };
+        
+    };
+    // console.log(select.home);
     return (
         <div className="footer-container">
-            <Link to='/' className='footer-btn'>< FiHome /></Link>
-            <Link to='/search' className='footer-btn'><FiSearch/></Link>
-            <Link to='/forecast' className='footer-btn'><MdAutoGraph/></Link>
-            <Link to='/settings' className='footer-btn'><FiSettings/></Link>
+            <Link
+                to="/"
+                className={`footer-btn ${select.home}`}
+                onClick={() => selected("home")}
+            >
+                <FiHome />
+            </Link>
+            <Link
+                to="/search"
+                className={`footer-btn ${select.search}`}
+                onClick={() => selected("search")}
+            >
+                <FiSearch />
+            </Link>
+            <Link
+                to="/forecast"
+                className={`footer-btn ${select.forecast}`}
+                onClick={() => selected("forecast")}
+            >
+                <MdAutoGraph />
+            </Link>
+            <Link
+                to="/settings"
+                className={`footer-btn ${select.settings}`}
+                onClick={() => selected("settings")}
+            >
+                <FiSettings />
+            </Link>
         </div>
-
-    )
-}
+    );
+};
 
 export default Footer;
